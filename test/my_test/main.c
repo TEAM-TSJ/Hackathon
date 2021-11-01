da325aa4efc5ad5599851a5237fd8efafca1cd10
junselee42 <junselee@student.42seoul.kr>
2021-11-01 19:04:08 +0900

    Tested sorting (recur)

--- test/my_test/main.c
+++ test/my_test/main.c
@@ -242,11 +242,44 @@ int fre_check(char value, t_list *fre)
 	return (-1);
 }
 
+t_list *ft_fre_sort(t_list *fre)
+{
+	t_list *tmp = 0;
+	int		node_cnt = 0;
+
+	tmp = fre;
+	while (tmp)
+	{
+		node_cnt++;
+		tmp = tmp->next;
+	}
+
+	tmp = fre;
+	int idx = -1;
+	while(++idx < node_cnt)
+	{
+		if (tmp->data > tmp->next->data)
+			;// 탑노드랑 넥스트노드를 스왑하는과정을 하는 함수를 cnt만큼 반복
+			//  뽑아낸 데이터는 바로바로 짤라서 해제; 만들어진 새로운 리스트는 추가후 다시 정렬(길이 다시쟤고)
+			//  그럼 결국ㄱ 프리솔트 함수 재귀함수.
+			//  하나 남을때까지
+			// 그럼 여기서 합쳐진 노드를 계속 들고있고, 그 정보는 리스트에 추가되고, 그게 합쳐질때 노드를 생성하고
+			// 하는 방식으로다가, 수행.
+			// 
+	}
+	printf("node size :%d\n", node_cnt);
+	return(fre);
+}
+
 t_node *ft_huffman(t_list *fre, char *str)
 {
+	//
 	t_node *t;
 	t_node *f;
+
+	// 가장작은 값 2개를 골라
 	t_list *min;
+	t_list *min2;
 
 	//빈도수가 낮은 노드를 뽑아내고
 	//while((min = min_fre(fre)) > 0)
@@ -254,8 +287,16 @@ t_node *ft_huffman(t_list *fre, char *str)
 
 	//min->count;//가장작은 빈도수가 될것임
 	//min->next->count;// 그다음 작은 빈도수가 될거임
-	
-	//노드를 하나생성해서 쟤네를 이어주고 트리를 만들어주는거에 넣어줘보자(널가드는 일단 신경쓰지말고)
+
+	/*
+	** 수정안 : 리스트에서 부모노드를 생성할때마다 부모노드를 기준으로하는 빈도수정렬을 다시 수행함
+	** 언제까지? 탐노드 하나남을때까지.
+	** 재귀 로직은 : 노드를 합치고 정렬이 다시된 리스트를 
+	*/
+
+	// 합치고 대체하게 하는거지 . 그렇지.
+
+	// 노드를 하나생성해서 쟤네를 이어주고 트리를 만들어주는거에 넣어줘보자(널가드는 일단 신경쓰지말고)
 	// 이렇게 하면 제일 작은거에 대해서 기본적으로 이어붙힐 뼈대가 된거잖아 그럼 크리에이트로 들어가서는
 	// 이제 재귀로 얘랑 min데이터를 하나씩 묶어주기만 하면 됨 언제까지? min데이터가 끝날때까지
 	f = (t_node *)malloc(sizeof(t_node));
@@ -264,11 +305,13 @@ t_node *ft_huffman(t_list *fre, char *str)
 
 	//프리퀀시에 이만큼에 문자가 없으면 터질수밖에 없지(첫번째 세그폴트 지점)
 	//./test jjkkkdddsss
-	
+
 	f->left->data = min->next->count;
 	f->right->data = min->count;
 	f->data = min->count + min->next->count;
 
+	// 만약 f의 데이터값이(빈도수가) 보다 작은게 두개있으면 그두개를 부모노드로 묶어줌
+	// 두개의 부모노드가 
 
 	t = creat_tree(t, f, min->next->next);
 
@@ -302,6 +345,7 @@ t_node *ft_huffman(t_list *fre, char *str)
 ** 정렬 규칙 : 모든 집합을 빈도순으로 구분해 나열한다.
 ** 정복 규칙 : 빈도가 가장 낮은 2개의 그룹을 합친다. 빈도수가 큰 그룹에 0을 주고 빈도수가 작은 그룹에 1을 준다.
 ** 리스트 빈도순으로 정렬할것.
+** 디코드는 결국 트리를 따라가는 
 */
 int	main(int ac, char **av)
 {
@@ -311,19 +355,18 @@ int	main(int ac, char **av)
 	
 	if (ac != 2)
 		return (ft_exit(1));
-	//만약 aaabbc가 들어왔다고 가정하고 뭘해야할지 생각해보자.
-
+	// 만약 aaabbc가 들어왔다고 가정하고 뭘해야할지 생각해보자.
 	// 문자당 빈도수가 들어있다. 정렬할 필요는 없을거같은게
 	// 문자는 av를 그대로 활용하면 되잖아. 그냥 빈도수 낮은거찾아서 집어넣자.
 	// 집어넣은거는 빈도수 음수로 바꿔주면 되자너, 버블솔트 구현해논거 가져올수도있는데, 이게 나을거같음.
 	fre = ft_frequency(av[1]);
 	
 	//일단 제대로 저장되나 찍어보자 viewer
-
+	ft_fre_sort(fre);
 	//난 문자별로 빈도수가 적힌 리스트가 있고(fre), 원래 문자열을 알고있어(av)
 	//허프만에서는 쟤네를 이진트리로 집어넣어야해, 빈도수가 낮은게 1오른쪽자식 높은게 0왼쪽자식이야.
-	ft_huffman(fre, av[1]);
-	frequency_viewer(fre);
+	//ft_huffman(fre, av[1]);
+	// frequency_viewer(fre);
 	
 	//printf("top value: %d", t->data);
 	// run_tree(&t);
